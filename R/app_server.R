fbc80e122f1553dc8e373a69b5030e3d1e4fe584
Weronika Puchała <ciemnamateria@gmail.com>
2024-01-04 16:51:49 +0100

    forgotten coma - sad face

--- R/app_server.R
+++ R/app_server.R
@@ -422,7 +422,7 @@ app_server <- function(input, output, session) {
   output[["download_fit_params_table"]] <- downloadHandler(
     filename = function(){
       paste0("fit_data_", fit_protein(),"-", fit_state(), ".csv")
-    }
+    },
     content = function(file){
       write.csv(dplyr::select(list_params(), -id), file)
     }
